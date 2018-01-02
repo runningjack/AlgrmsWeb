fc5be98493e12d7381ba9cc238e311e4c6f83e22
runningjack <amedora09@gmail.com>
2018-01-02 08:50:07 +0100

    jquery ui manage and admin controller added

---
+++ AlgrmsWeb/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlgrmsWeb.Startup))]
+namespace AlgrmsWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+        
+    }
+}
